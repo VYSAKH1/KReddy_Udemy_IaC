09286d726bdb2b3b958c33f40884dfda0050a83b
VYSAKH1 <vysakhvalsan90@gmail.com>
2022-11-11 20:06:46 +0530

    first commit

---
+++ 10-Azure-Virtual-Network-4Tier/vnet.tf
@@ -0,0 +1,6 @@
+resource "azurerm_virtual_network" "vnet" {
+  name = "${local.resource_name_prefix}-${var.vnet_name}"
+  address_space = var.vnet_address_space
+  location = azurerm_resource_group.rg.location
+  
+}

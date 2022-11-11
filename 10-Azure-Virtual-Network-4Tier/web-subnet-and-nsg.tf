28283025b3c282bf5bd9645a11456b4a3b01bab7
VYSAKH1 <vysakhvalsan90@gmail.com>
2022-11-11 20:28:14 +0530

    Corrected web subnet name

--- 10-Azure-Virtual-Network-4Tier/web-subnet-and-nsg.tf
+++ 10-Azure-Virtual-Network-4Tier/web-subnet-and-nsg.tf
@@ -6,7 +6,7 @@ resource "azurerm_subnet" "web_subnet" {
 }
 
 resource "azurerm_network_security_group" "web_subnet_nsg" {
-  name                = "${azurerm_subnet.websubnet.name}-nsg"
+  name                = "${azurerm_subnet.web_subnet.name}-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
